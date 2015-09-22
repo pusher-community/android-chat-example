18a517b9b1cfe3808e9241b8a38fd24a3b1784fd
Jamie Patel <jamie@notespublication.com>
2015-09-22 23:44:54 +0100

    Sending messages

--- app/src/main/java/com/example/jamiepatel/pusherchat/MainActivity.java
+++ app/src/main/java/com/example/jamiepatel/pusherchat/MainActivity.java
@@ -3,26 +3,39 @@ package com.example.jamiepatel.pusherchat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.pusher.client.Pusher;
 import com.pusher.client.channel.Channel;
 import com.pusher.client.channel.SubscriptionEventListener;
 import com.google.gson.Gson;
 
+import org.apache.http.entity.StringEntity;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnKeyListener {
 
     MessageAdapter messageAdapter;
+    EditText messageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageInput = (EditText) findViewById(R.id.message_input);
+
+        messageInput.setOnKeyListener(this);
 
         messageAdapter = new MessageAdapter(this, new ArrayList<Message>());
         final ListView messagesView = (ListView) findViewById(R.id.messages_view);
@@ -74,5 +87,39 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_ENTER){
+            String text = messageInput.getText().toString();
+            postMessage(text);
+        }
+        return true;
+    }
+
+    private void postMessage(String text)  {
+        Message message = new Message();
+        message.body = text;
+        message.sender = "jamie patel";
+
+        Gson gson = new Gson();
+        String body = gson.toJson(message);
+
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(body);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.post(this, "http://yolo.ngrok.com/messages", entity, "application/json", new JsonHttpResponseHandler(){
+
+        });
     }
 }
